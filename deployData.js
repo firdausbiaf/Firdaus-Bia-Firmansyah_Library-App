795caa3a87e8ca59b24e8582bcfebbe57cc3006b
firdausbiaf <=>
2023-06-29 00:25:07 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Firdaus Bia Firmansyah'; // TODO: replace this
+
+const studentId = 'FE5299458'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://fe5299458-firdaus-bia-firmansyah.netlify.app/client/index.html'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
